28cd076d034199b7ddd9edc7e46afb87689552ef
Aimen <aimen.el-sayed@stud.fh-campuswien.ac.at>
2024-09-10 18:55:58 +0200

    test ci

--- src/app/error/page.tsx
+++ src/app/error/page.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
 };
 
 const ErrorPage = () => {
-  return (
+   return (
     <>
       <section className="relative z-10 pb-16 pt-36 md:pb-20 lg:pb-28 lg:pt-[180px]">
         <div className="container">
